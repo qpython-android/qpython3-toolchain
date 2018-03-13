27f0cc9a128ec85d8775f02b58965a0f4325f33e
River <riverfor@gmail.com>
2018-03-13 20:26:55 +0800

    Update qpython*.sh

--- mk/qpyc3/bin/qpython3-android5.sh
+++ mk/qpyc3/bin/qpython3-android5.sh
@@ -1,3 +1,24 @@
 #!/system/bin/sh
 DIR=${0%/*}
-. $DIR/init.sh && $DIR/python3-android5 "$@" && $DIR/end.sh
+
+if [ -z "$1" ]; then
+
+    . $DIR/init.sh && $DIR/python3-android5 && $DIR/bend.sh
+
+else
+
+    is_web=`grep "#qpy:webapp" $1`
+    is_qapp1=`grep "#qpy:qpyapp" $1`
+    is_qapp2=`grep "#qpy:qpysrv" $1`
+
+    if [ -z "${is_web}" ]  && [ -z "${is_qapp1}" ] && [ -z "${is_qapp2}" ]; then
+
+        . $DIR/init.sh && $DIR/python3-android5 "$@" && $DIR/bend.sh
+
+    else
+
+        . $DIR/init.sh && $DIR/python3-android5 "$@" && $DIR/send.sh
+
+    fi
+
+fi
--- mk/qpyc3/bin/qpython3-root.sh
+++ mk/qpyc3/bin/qpython3-root.sh
@@ -1,6 +1,20 @@
 #!/system/bin/sh
 DIR=${0%/*}
-test=$DIR'/init.sh && '$DIR'/python3 '"$@"' && '$DIR'/end.sh'
+if [ -z "$1" ]; then
+    test=$DIR'/init.sh && '$DIR'/python3 && '$DIR'/bend.sh'
+else
+    is_web=`grep "#qpy:webapp" $1`
+    is_qapp1=`grep "#qpy:qpyapp" $1`
+    is_qapp2=`grep "#qpy:qpysrv" $1`
+
+    if [ -z "${is_web}" ]  && [ -z "${is_qapp1}" ] && [ -z "${is_qapp2}" ]; then
+        test=$DIR'/init.sh && '$DIR'/python3 '"$@"' && '$DIR'/bend.sh'
+    else
+        test=$DIR'/init.sh && '$DIR'/python3 '"$@"' && '$DIR'/send.sh'
+    fi
+fi
+
+
 cat $DIR/init.sh > $DIR/python3-root
 echo '\n' >> $DIR/python3-root
 echo $test >> $DIR/python3-root
