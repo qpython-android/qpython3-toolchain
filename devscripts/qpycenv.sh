4339ec37a005150516c9a9f5b22d64cbfa0d4ff8
river <riverfor@gmail.com>
2018-05-11 23:08:56 -0400

    Change NDK value

--- devscripts/qpycenv.sh
+++ devscripts/qpycenv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export ANDROID_NDK=/Users/yanhecun/Develop/android-ndk-r16b
+export ANDROID_NDK=/home/QPY/android-ndk-r13b
 export ANDROID_VER=21
 export TARGET_ARCH_QPY=armeabi-v7a
 
@@ -25,5 +25,6 @@ export CXX="arm-linux-androideabi-g++ ${CC_FLAGS_QPY} ${LDFLAGS}"
 
 export RANLIB="arm-linux-androideabi-ranlib"
 export AR="arm-linux-androideabi-ar"
+export AS="arm-linux-androideabi-as"
 export LD="arm-linux-androideabi-ld"
 #export LDSHARED="arm-linux-androideabi-ld ${LDFLAGS} -shared"
