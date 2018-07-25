3c48c2a45f31fa1b397d45299505746acd002b42
river <riverfor@gmail.com>
2018-07-25 08:11:46 -0400

    Compile numpy and scipy OK

--- pybuild/packages/scipy.py
+++ pybuild/packages/scipy.py
@@ -17,7 +17,7 @@ class SciPy(Package):
     def build(self):
         import os,sys
         self.system( 
-            f'python setup.py build_ext -I../../build/target/python/usr/include/python3.6m:../../build/target/openblas/usr/include:{self.env["ANDROID_NDK"]}/sources/cxx-stl/gnu-libstdc++/4.9/include:{self.env["ANDROID_NDK"]}/sources/cxx-stl/gnu-libstdc++/4.9/libs/armeabi/include:{self.env["ANDROID_NDK"]}/sources/cxx-stl/gnu-libstdc++/4.9/libs/armeabi-v7a/include -L../../build/target/python/usr/lib:../../build/target/openblas/usr/lib:../numpy/build/temp.linux-x86_64-3.6 -lopenblas,python3.6m,m,gcc' 
+            f'LDFLAGS=\" -shared -DANDROID --sysroot {self.env["ANDROID_NDK"]}/platforms/android-21/arch-arm -L{self.env["ANDROID_NDK_GFORTRAN"]}/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/arm-linux-androideabi/lib/armv7-a\" python setup.py build_ext -I../../build/target/python/usr/include/python3.6m:../../build/target/openblas/usr/include:{self.env["ANDROID_NDK"]}/sources/cxx-stl/gnu-libstdc++/4.9/include:{self.env["ANDROID_NDK"]}/sources/cxx-stl/gnu-libstdc++/4.9/libs/armeabi-v7a/include -L../../build/target/python/usr/lib:../../build/target/openblas/usr/lib:../numpy/build/temp.linux-x86_64-3.6:{self.env["ANDROID_NDK_GFORTRAN"]}/platforms/android-21/arch-arm/usr/lib:{self.env["ANDROID_NDK_GFORTRAN"]}/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/arm-linux-androideabi/lib/armv7-a -lopenblas,python3.6m,m,gcc,gfortran' 
         )
         self.run([
             'python',
